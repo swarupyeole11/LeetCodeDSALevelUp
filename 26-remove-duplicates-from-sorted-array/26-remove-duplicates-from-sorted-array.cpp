b7b4c19d06ebcee539bfe8ba6c9d58101d53fd66
swarupcode-prog <76248886+swarupcode-prog@users.noreply.github.com>
2022-03-16 12:10:20 +0530

    Time: 24 ms (27.32%), Space: 18.3 MB (92.84%) - LeetHub

---
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+     
+//       if(nums.size()<=1)
+//       {
+//          return nums.size();
+//       }
+      int n  = nums.size();
+        
+//      for(int i=0;i<n;i++)
+//      {
+          
+//               if(i!=0 && nums[i]==nums[i-1] )  //first you sould always check i!=0 condition because if not then you will be                                                       //acessing out of bound element
+//               {    
+                   
+//                    int x=nums[i];
+//                    cout<<"x:"<<x<<" ";
+                  
+//                    for(int j=i;j<n-1;j++)
+//                    {   
+                       
+//                       // cout<<"inside :"<<j<<" ";
+//                        nums[j]=nums[j+1];
+//                    }
+//                    nums[n-1]=x;
+//               }
+                  
+          
+//      } 
+//         int count=0;
+        
+//         for(int i=0;i<n;i++)
+//           cout<<nums[i];
+        
+        
+//         for(int i=0;i<n;i++)
+//         {   
+//            // cout<<nums[i];
+//             if(i!=n-1&&nums[i]>=nums[i+1])
+//             {
+//                 return count+1; //1 2 3 4 5 6 
+//             }
+            
+//             count++;
+//         }
+      
+//         return count;
+        
+        int count = 0;
+for(int i = 1; i < n; i++){
+    if(nums[i] == nums[i-1]) count++;
+    else nums[i-count] = nums[i];
+}
+return n-count;
+    }
+};
